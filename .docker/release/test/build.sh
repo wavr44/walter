9f3a47ed5343c5590cc201dd96ecf615211e4da3
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-04-14 08:55:46 +0000

    Push only if image test succeeds

--- .docker/release/test/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -eu
-docker build -t snappymail/snappymail:test -f .docker/release/Dockerfile .
--- .docker/release/test/test.sh
+++ .docker/release/test/test.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # This script tests a given docker image using https://github.com/GoogleContainerTools/container-structure-test
 set -eu
-
-IMAGE=${1:-snappymail/snappymail:test}
 SCRIPT_DIR=$( cd "$(dirname "$0")" && pwd )
-docker inspect "$IMAGE" > /dev/null 2>&1 || docker pull "$IMAGE" # Pull the image if it doesn't exist in docker daemon
+IMAGE=${1:-}
+echo "Testing image: $IMAGE"
 docker run --rm -i \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v "$SCRIPT_DIR/config.yaml:/config.yaml:ro" \
