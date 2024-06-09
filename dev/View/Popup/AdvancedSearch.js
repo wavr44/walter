a3fb5a30eed06b778b3dbb0270c00f05ba72da50
the-djmaze <>
2024-06-09 17:14:13 +0200

    Translation strings for #1606

--- dev/View/Popup/AdvancedSearch.js
+++ dev/View/Popup/AdvancedSearch.js
@@ -59,21 +59,21 @@ export class AdvancedSearchPopupView extends AbstractViewPopup {
 				// and adjust the prefix accordingly
 				// let prefix_since = 'SEARCH/SINCE_DATE_';
 				let prefix = 'SEARCH/SINCE_';
-				let prefix_before = 'SEARCH/BEFORE_DATE_';
+				let prefix_before = 'SEARCH/BEFORE_';
 				return [
-					{ id: -365, name: i18n(prefix_before + 'YEAR') },
-					{ id: -180, name: i18n(prefix_before + '6_MONTHS') },
-					{ id: -90, name: i18n(prefix_before + '3_MONTHS') },
-					{ id: -30, name: i18n(prefix_before + 'MONTH') },
-					{ id: -7, name: i18n(prefix_before + '7_DAYS') },
-					{ id: -3, name: i18n(prefix_before + '3_DAYS') },
 					{ id: 0, name: i18n('SEARCH/DATE_ALL') },
 					{ id: 3, name: i18n(prefix + '3_DAYS') },
 					{ id: 7, name: i18n(prefix + '7_DAYS') },
 					{ id: 30, name: i18n(prefix + 'MONTH') },
 					{ id: 90, name: i18n(prefix + '3_MONTHS') },
 					{ id: 180, name: i18n(prefix + '6_MONTHS') },
-					{ id: 365, name: i18n(prefix + 'YEAR') }
+					{ id: 365, name: i18n(prefix + 'YEAR') },
+					{ id: -3, name: i18n(prefix_before + '3_DAYS') },
+					{ id: -7, name: i18n(prefix_before + '7_DAYS') },
+					{ id: -30, name: i18n(prefix_before + 'MONTH') },
+					{ id: -90, name: i18n(prefix_before + '3_MONTHS') },
+					{ id: -180, name: i18n(prefix_before + '6_MONTHS') },
+					{ id: -365, name: i18n(prefix_before + 'YEAR') }
 				];
 			},
 
