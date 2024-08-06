2f90d95c4c3ef9a533775116a9ae09ba5f6c8ee8
the-djmaze <>
2024-08-06 13:40:43 +0200

    Set author for #1673

--- plugins/search-filters/index.php
+++ plugins/search-filters/index.php
@@ -4,7 +4,9 @@ class SearchFiltersPlugin extends \RainLoop\Plugins\AbstractPlugin
 {
 	public const
 		NAME = 'Search Filters',
-		VERSION = '0.0.2',
+		AUTHOR = 'AbdoBnHesham',
+		URL    = 'https://github.com/the-djmaze/snappymail/pull/1673',
+		VERSION = '0.2',
 		RELEASE = '2024-06-28',
 		REQUIRED = '2.36.3',
 		CATEGORY = 'General',
